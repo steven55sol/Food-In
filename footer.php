1944402c320682f239f386c34084b0ce7f67a634
steven55sol <stiven.sol5cinco@gmail.com>
2021-01-05 07:11:09 -0500

    Pagina web de comidas saludables

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="cont-footer">
+        <p>&copy; Derechos Reservados de Food_Innovation 2020-<?php  date('Y') ?></p>
+    </footer>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+    <link rel="stylesheet" href="Css/estilo.css">
+    <title>Food_In</title>
+</head>
+<body>
+    <header class="container-header">
+        <img src="icon/food.png" alt="">
+    </header>
+    <nav class="menu">
+        <ul>
+            <li><a href="" class="select"><i class="fas fa-home"></i>Home</a></li>
+            <li><a href=""><i class="fas fa-apple-alt"></i>Comidas saludables</a></li>
+            <li><a href=""><i class="fas fa-cocktail"></i>Bebidas saludables</a></li>
+            <li><a href=""><i class="fas fa-cheese"></i>Postres</a></li>
+        </ul>
+    </nav>
